119a0d89308c3a27985d07854e5e78e1c9dc092e
Nadja <nc1192@gmail.com>
2023-01-12 19:11:18 -0500

    Voice Bot 1

--- Bot.py
+++ Bot.py
@@ -9,26 +9,30 @@ load_dotenv()
 intents=discord.Intents.all()
 client = discord.Client(intents=intents)
 token = os.getenv('TOKEN')
-#os.chdir("C:\Users\\nc119\Desktop\Test Bot")
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-""""
 @commands.command()
 async def check_voice(ctx):
-    voice_channel = ctx.author.voicestate.channel
-    if voice_channel == None:
+    try:
+        voice_channel = ctx.author.voice.channel
+    except:
         await ctx.channel.send('Connect to VC')
     else:
         connection = await voice_channel.connect()
-        connection.play(client.FFmpegAudio(source="./Desktop/Bot/sample.mp3", executable="./Desktop/ffmpeg/bin/ffmpeg.exe", 
-            after=lambda e: connection.disconnect()))
-        #check if lambda is necessary, otherwise run disconnect after play
-"""
-
-
+        connection.play(discord.FFmpegPCMAudio(source="./Desktop/Bot/sample.mp3", executable="./Desktop/ffmpeg/bin/ffmpeg.exe"), after=lambda e: print('Test text'))
+        while connection.is_playing():
+            time.sleep(0.1)
+        await connection.disconnect()
+        
+          
+@commands.command()
+async def voice_dc(ctx):
+    vc = ctx.voice_client
+    await vc.disconnect()
  
 @client.event
 async def on_message(message):
@@ -37,16 +41,13 @@ async def on_message(message):
  
     if message.content.lower() == 'fernando':
         await message.channel.send('@Lerggio#5880 What day is it?')
-        #await message.channel.send(file=discord.File('Frog.png'))
         with open('./Desktop/Bot/frog.png', 'rb') as f:
             picture = discord.File(f)
             await message.channel.send(file=picture)
         return
 
     if message.content.lower() == 'jordan':
-        await check_voice(client.user)
-
-
-
+        await check_voice(message)
+        #await voice_dc(client.user)
 
 client.run(token)
