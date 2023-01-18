f422c7a60ae4f6c1ee75ad4ae9056f62ea18c07d
Nadja <nc1192@gmail.com>
2023-01-18 01:14:59 -0500

    Used Lambda for After

--- Bot.py
+++ Bot.py
@@ -1,6 +1,6 @@
 import os
 import discord
-import time
+import asyncio
 from discord.ext import commands  
 from dotenv import load_dotenv
 
@@ -15,66 +15,31 @@ bot = discord.ext.commands.Bot(command_prefix = '!', intents = intents)
 
 @bot.event
 async def on_ready():
-    print('We have logged in!')
+    print('We have logged in!') 
 
 @bot.command()
 async def jordan(ctx):
+    source = 'jordan.mp3'
     try:
         voice_channel = ctx.author.voice.channel
     except:
         await ctx.channel.send('Connect to VC')
     else:
-        connection = await voice_channel.connect()
-        connection.play(discord.FFmpegPCMAudio(source = "./Desktop/Bot/sample.mp3", executable = "./Desktop/ffmpeg/bin/ffmpeg.exe"), after=lambda e: print('Test text'))
-        while connection.is_playing():
-            time.sleep(0.1)
-        await connection.disconnect()
+        try:
+            connection = await voice_channel.connect()
+        except:
+            print('Already connected')
+        else:
+            connection.play(discord.FFmpegPCMAudio(source = source, executable = "ffmpeg"), 
+                after = lambda _ : asyncio.run_coroutine_threadsafe(coro=connection.disconnect(), loop = connection.loop))
 
 @bot.command()
 async def fernando(ctx):
     await ctx.send('@Lerggio#5880 What day is it?')
-    with open('./Desktop/Bot/frog.png', 'rb') as f:
+    with open('frog.png', 'rb') as f:
         picture = discord.File(f)
         await ctx.send(file=picture)
     return
 
-''''
-@commands.command()
-async def check_voice(ctx):
-    try:
-        voice_channel = ctx.author.voice.channel
-    except:
-        await ctx.channel.send('Connect to VC')
-    else:
-        connection = await voice_channel.connect()
-        connection.play(discord.FFmpegPCMAudio(source="./Desktop/Bot/sample.mp3", executable="./Desktop/ffmpeg/bin/ffmpeg.exe"), after=lambda e: print('Test text'))
-        while connection.is_playing():
-            time.sleep(0.1)
-        await connection.disconnect()
-
-        
-          
-@commands.command()
-async def voice_dc(ctx):
-    vc = ctx.voice_client
-    await vc.disconnect()
- 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
- 
-    if message.content.lower() == 'fernando':
-        await message.channel.send('@Lerggio#5880 What day is it?')
-        with open('./Desktop/Bot/frog.png', 'rb') as f:
-            picture = discord.File(f)
-            await message.channel.send(file=picture)
-        return
-
-    if message.content.lower() == 'jordan':
-        await check_voice(message)
-        #await voice_dc(client.user)
-
-'''
 bot.run(token)
 #client.run(token)
