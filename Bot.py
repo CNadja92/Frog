b69561ab316da3a16bcee2eac192bf77643bbae2
Nadja <nc1192@gmail.com>
2023-01-18 11:50:23 -0500

    Added Fernando mp3

--- Bot.py
+++ Bot.py
@@ -7,7 +7,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 intents = discord.Intents.all()
-#client = discord.Client(intents=intents)
 token = os.getenv('TOKEN')
 
 bot = discord.ext.commands.Bot(command_prefix = '!', intents = intents)
@@ -37,6 +36,25 @@ async def jordan(ctx):
 
 @bot.command()
 async def fernando(ctx):
+    source = 'fernando.mp3'
+    voice_client = discord.utils.get(bot.voice_clients)
+
+    if voice_client:
+        await ctx.send('Connected to voice channel already')
+        return
+
+    try:
+        voice_channel = ctx.author.voice.channel
+        voice_client = await voice_channel.connect()
+    except:
+        await ctx.channel.send('Connect to VC')
+    else:
+        voice_client.play(discord.FFmpegPCMAudio(source = source, executable = "ffmpeg"), 
+                after = lambda _ : asyncio.run_coroutine_threadsafe(coro=voice_client.disconnect(), loop = voice_client.loop))
+
+
+@bot.command()
+async def wednesday(ctx):
     await ctx.send('@Lerggio#5880 What day is it?')
     with open('frog.png', 'rb') as f:
         picture = discord.File(f)
@@ -44,4 +62,3 @@ async def fernando(ctx):
     return
 
 bot.run(token)
-#client.run(token)
