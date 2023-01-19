3bac88f4f9c92119efd4b4da648c99adcc81c291
Nadja <nc1192@gmail.com>
2023-01-18 21:55:12 -0500

    Added Jordan Cool

--- Bot.py
+++ Bot.py
@@ -17,8 +17,26 @@ async def on_ready():
     print('We have logged in!') 
 
 @bot.command()
-async def jordan(ctx):
-    source = 'jordan.mp3'
+async def jordanpoo(ctx):
+    source = 'jordanpoo.mp3'
+    voice_client = discord.utils.get(bot.voice_clients)
+
+    if voice_client:
+        await ctx.send('Connected to voice channel already')
+        return
+
+    try:
+        voice_channel = ctx.author.voice.channel
+        voice_client = await voice_channel.connect()
+    except:
+        await ctx.channel.send('Connect to VC')
+    else:
+        voice_client.play(discord.FFmpegPCMAudio(source = source, executable = "ffmpeg"), 
+                after = lambda _ : asyncio.run_coroutine_threadsafe(coro=voice_client.disconnect(), loop = voice_client.loop))
+
+@bot.command()
+async def jordancool(ctx):
+    source = 'jordancool.mp3'
     voice_client = discord.utils.get(bot.voice_clients)
 
     if voice_client:
@@ -55,7 +73,7 @@ async def fernando(ctx):
 
 @bot.command()
 async def wednesday(ctx):
-    await ctx.send('@Lerggio#5880 What day is it?')
+    await ctx.send('What day is it?')
     with open('frog.png', 'rb') as f:
         picture = discord.File(f)
         await ctx.send(file=picture)
