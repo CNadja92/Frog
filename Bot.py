6c326e07f753b9ce35940ed875df0b291fc333fc
Nadja <nc1192@gmail.com>
2023-02-15 19:06:16 -0500

    Adding Async Task to Check for One Piece Update

--- Bot.py
+++ Bot.py
@@ -2,17 +2,23 @@ import os
 import discord
 import asyncio
 import disnake
+import requests
+import sqlite3
+from bs4 import BeautifulSoup
 from disnake.ext import commands
 from discord.ext import commands  
 from dotenv import load_dotenv
 
 load_dotenv()
 
+# sets intents and env token
 intents = discord.Intents.all()
 token = os.getenv('TOKEN')
 
+# sets command prefix
 bot = discord.ext.commands.Bot(command_prefix = '!', intents = intents)
 
+# help command configuration
 class MyHelp(commands.HelpCommand):
     async def send_bot_help(self, mapping):
         filtered = await self.filter_commands(self.context.bot.commands, sort = True)  # returns a list of command objects
@@ -29,7 +35,45 @@ bot.help_command = MyHelp()
 
 @bot.event
 async def on_ready():
-    print('We have logged in!') 
+    print('We have logged in!')
+
+async def one_piece():
+    await bot.wait_until_ready()
+
+    # get text channel
+    channel_id = 1062734654634999861
+    channel = bot.get_channel(channel_id)
+    Shiba_id = 245706289550852098
+
+    while not bot.is_closed():
+        
+        # connect to database to get chapter number
+        conn = sqlite3.connect('data.db') # connects to database
+        cursor = conn.cursor() # creates a cursor to execute SQL queries
+        cursor.execute("SELECT ChapterNum FROM MangaChapter WHERE Name='One Piece'")
+        result = cursor.fetchone()[0] # fetch the query result
+
+        # get html content for one piece
+        response = requests.get('https://onepiecechapters.com/mangas/5/one-piece')
+        html_content = response.content
+        soup = BeautifulSoup(html_content, 'html.parser')
+        chapter = '-' + str(result)
+
+        # find href with manga chapter number
+        link = soup.find('a', href=lambda href: href and chapter in str(href))
+        if link is not None: # if href exists, post link and update database
+            href_value = link['href']
+            await channel.send('https://onepiecechapters.com' + href_value)
+            next_ch = result+1
+            cursor.execute("UPDATE MangaChapter SET ChapterNum = ? WHERE Name = ?", (next_ch, 'One Piece'))
+            conn.commit() # commit executed changes
+        # close the connection and cursor
+        cursor.close()
+        conn.close()
+
+        # run every 4 hours
+        print('Checking for the One Piece!')
+        await asyncio.sleep(14400)
 
 @bot.command()
 async def jordanpoo(ctx):
@@ -85,6 +129,41 @@ async def fernando(ctx):
         voice_client.play(discord.FFmpegPCMAudio(source = source, executable = "ffmpeg"), 
                 after = lambda _ : asyncio.run_coroutine_threadsafe(coro=voice_client.disconnect(), loop = voice_client.loop))
 
+@bot.command()
+async def coffee(ctx):
+    source = 'coffee.mp3'
+    voice_client = discord.utils.get(bot.voice_clients)
+
+    if voice_client:
+        await ctx.send('Connected to voice channel already')
+        return
+
+    try:
+        voice_channel = ctx.author.voice.channel
+        voice_client = await voice_channel.connect()
+    except:
+        await ctx.channel.send('Connect to VC')
+    else:
+        voice_client.play(discord.FFmpegPCMAudio(source = source, executable = "ffmpeg"), 
+                after = lambda _ : asyncio.run_coroutine_threadsafe(coro=voice_client.disconnect(), loop = voice_client.loop))
+
+@bot.command()
+async def chipotle(ctx):
+    source = 'chipotle.mp3'
+    voice_client = discord.utils.get(bot.voice_clients)
+
+    if voice_client:
+        await ctx.send('Connected to voice channel already')
+        return
+
+    try:
+        voice_channel = ctx.author.voice.channel
+        voice_client = await voice_channel.connect()
+    except:
+        await ctx.channel.send('Connect to VC')
+    else:
+        voice_client.play(discord.FFmpegPCMAudio(source = source, executable = "ffmpeg"), 
+                after = lambda _ : asyncio.run_coroutine_threadsafe(coro=voice_client.disconnect(), loop = voice_client.loop))
 
 @bot.command()
 async def wednesday(ctx):
@@ -94,4 +173,21 @@ async def wednesday(ctx):
         await ctx.send(file=picture)
     return
 
-bot.run(token)
+async def test():
+    channel = bot.get_channel(1062734654634999861)
+    await channel.send('Test')
+
+async def background_test():
+    await bot.wait_until_ready()
+    while not bot.is_closed():
+        await asyncio.sleep(30)
+        await test()
+
+
+async def start():
+    async with bot:
+        bot.loop.create_task(one_piece())
+        await bot.start(token)
+
+#bot.run(token)
+asyncio.run(start())
---
+++ test.sql
@@ -0,0 +1,6 @@
+-- CREATE TABLE MangaChapter(Name VARCHAR(255), ChapterNum INT)
+
+-- INSERT INTO MangaChapter(Name, ChapterNum)
+-- VALUES ('One Piece', 1074)
+
+SELECT * FROM MangaChapter;
